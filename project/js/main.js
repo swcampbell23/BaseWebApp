e4d84587b528a7eca7c174557522158cf76f6c64
Steven Campbell <steven@Stevens-MBP.attlocal.net>
2022-06-26 14:41:17 -0500

    Made my Final Change I hope

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,22 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+
+function getweather(searchQuery) {
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&appid="+apiKey;
+
+  $(".city").text("");
+  $(".temp").text("");
+
+  $.ajax(url,{success: function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+
+  }, error: function(error){
+    $(".error-message").text("An error occured");
+  }})
+
+}
+
+function searchweather() {
+  var searchQuery = $(".search").val();
+  getweather(searchQuery);
 }
